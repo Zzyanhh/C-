6a5ca71698efeca67f710a99c3de87de3a1c84b1
ZzzzAsuraYy <98115217+Zzyanhh@users.noreply.github.com>
2025-03-17 21:51:04 +0800

    Add files via upload

---
+++ 3.14.cpp
@@ -0,0 +1,18 @@
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	int auks, bats, coots;
+	auks = 19.99 + 11.99;
+	bats = (int)19.99 + (int)11.99;
+	coots = int(19.99) + int(11.99);
+	cout << "auks = " << auks << ", bats = " << bats << ", coots = " << coots << endl;
+
+	char ch = 'z';
+	cout << "the code for " << ch << " is " << int(ch) << endl;
+	cout << static_cast<int>(ch) << endl;
+
+	return 0;
+}
+*/
