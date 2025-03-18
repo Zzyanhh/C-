2a04374940fd57c7cd39678c7006b47530cffe6a
ZzzzAsuraYy <98115217+Zzyanhh@users.noreply.github.com>
2025-03-18 18:34:32 +0800

    Add files via upload

---
+++ 4.15.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int updates = 6;
+	int* p_updates;
+	p_updates = &updates;
+
+	cout << "value: updates = " << updates;
+	cout << ",*p_updates = " << *p_updates << endl;
+
+	cout << "addresses: &updates = " << &updates;
+	cout << ",p_updates = " << p_updates << endl;
+
+	*p_updates = *p_updates + 1;
+	cout << "now updates = " << updates << endl;
+
+	return 0;
+}
+*/
---
+++ 4.16.cpp
@@ -0,0 +1,15 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int higgens = 5;
+	int* pt = &higgens;
+
+	cout << "value of higgens = " << higgens << "; address of higgens = " << &higgens << endl;
+	cout << "value of *pt = " << *pt << "; value of pt = " << pt << endl;
+
+	return 0;
+}
+*/
